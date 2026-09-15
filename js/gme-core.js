b9bcd406ad83f064895a404b4962cd266c662f17
zknhector <37241827+zknhector@users.noreply.github.com>
2026-09-16 00:45:43 +0900

    Refactor GMECore methods and remove unused code

--- js/gme-core.js
+++ js/gme-core.js
@@ -9,249 +9,46 @@
 
 
 const GMECore = {
-
-
-    module:null,
-
-    emulator:null,
-
-    ready:false,
-
-
-
-
-
-    /*
-      初期化
-    */
-
-    async init(){
-
-
-        console.log(
-            "GME Core initialize"
-        );
-
-
-
-        if(
-            window.Module
-        ){
-
-
-            this.module =
-                window.Module;
-
-
-
-            this.ready=true;
-
-
-
-            console.log(
-                "GME Core ready"
-            );
-
-
-            return true;
-
-
-        }
-
-
-
-        console.warn(
-            "Emscripten Module waiting"
-        );
-
-
-
-        return false;
-
-
-    },
-
-
-
-
-
-
-
-
-    /*
-      NSFオープン
-
-    */
-
-    open(buffer){
-
-
-
-        if(
-            !this.ready
-        ){
-
-
-            console.warn(
-                "GME not ready"
-            );
-
-
-            return false;
-
-        }
-
-
-
-
-        /*
-          本接続予定:
-
-          gme_open_data(
-             data,
-             length,
-             &emu
-          )
-
-        */
-
-
-
-        this.emulator =
-        {
-
-            data:buffer,
-
-            track:0
-
-
-        };
-
-
-
-        console.log(
-            "NSF opened"
-        );
-
-
-
-        return true;
-
-
-    },
-
-
-
-
-
-
-
-
-    /*
-      トラック開始
-
-    */
-
-    startTrack(track){
-
-
-
-        if(
-            !this.emulator
-        ){
-
-            return false;
-
-        }
-
-
-
-        this.emulator.track =
-            track;
-
-
-
-        /*
-          gme_start_track()
-
-        */
-
-
-
-        console.log(
-            "Track start",
-            track
-        );
-
-
-
-        return true;
-
-
-    },
-
-
-
-
-
-
-
-
-    /*
-      PCM取得
-
-    */
-
-    getSamples(length){
-
-
-
-        /*
-          gme_play()
-
-        */
-
-
-
-        return new Int16Array(
-            length
-        );
-
-
-    },
-
-
-
-
-
-
-
-
-    /*
-      停止
-
-    */
-
-    stop(){
-
-
-
-        this.emulator =
-            null;
-
-
-
+  ready: false,
+
+  async init() {
+    this.ready = await GMELoader.init();
+    return this.ready;
+  },
+
+  async open(buffer) {
+    if (!this.ready && !(await this.init())) {
+      return false;
     }
 
+    await GMELoader.openNSF(buffer);
 
+    return true;
+  },
 
+  getTrackCount() {
+    return GMELoader.getTrackCount();
+  },
 
+  getTrackInfo(track) {
+    return GMELoader.getTrackInfo(track);
+  },
 
+  startTrack(track) {
+    return GMELoader.start(track) === 1;
+  },
+
+  getSamples(length) {
+    return GMELoader.getPCM(length);
+  },
+
+  stop() {
+    GMELoader.stop();
+  },
+
+  close() {
+    GMELoader.close();
+  }
 };
 
-
-
-
-
-window.GMECore =
-    GMECore;
+window.GMECore = GMECore;
