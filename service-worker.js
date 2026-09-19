8eb17f7e9915c1385d808910b4ad6ced69b7327f
zknhector <37241827+zknhector@users.noreply.github.com>
2026-09-20 00:22:52 +0900

    Update service worker to version 0.5

--- service-worker.js
+++ service-worker.js
@@ -1,75 +1,76 @@
-const CACHE_NAME = "famicompo-nsf-player-v0.4";
+const CACHE_NAME = "famicompo-nsf-player-v0.5";
 
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-
-  "./css/style.css",
-
-  "./js/app.js",
-  "./js/library.js",
-  "./js/nsf-parser.js",
-  "./js/nsf-engine.js",
-  "./js/player.js",
-  "./js/audio-worker.js",
-  "./js/gme-core.js",
-  "./js/gme-loader.js",
-  "./js/libgme-bridge.js",
-
-  "./wasm/gme.js",
-  "./wasm/gme.wasm"
+"./",
+"./index.html",
+"./manifest.json",
+"./css/style.css",
+"./js/app.js",
+"./js/library.js",
+"./js/nsf-parser.js",
+"./js/nsf-engine.js",
+"./js/player.js",
+"./js/audio-worker.js",
+"./js/gme-core.js",
+"./js/gme-loader.js",
+"./js/libgme-bridge.js",
+"./wasm/gme.js",
+"./wasm/gme.wasm"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(ASSETS))
+.then(() => self.skipWaiting())
+);
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys =>
-        Promise.all(
-          keys
-            .filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-        )
-      )
-      .then(() => self.clients.claim())
-  );
+event.waitUntil(
+caches.keys()
+.then(keys =>
+Promise.all(
+keys
+.filter(key => key !== CACHE_NAME)
+.map(key => caches.delete(key))
+)
+)
+.then(() => self.clients.claim())
+);
 });
 
 self.addEventListener("fetch", event => {
-  const request = event.request;
+const request = event.request;
 
-  if (request.method !== "GET") {
-    return;
-  }
+if (request.method !== "GET") {
+return;
+}
 
-  event.respondWith(
-    caches.match(request)
-      .then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+event.respondWith(
+caches.match(request)
+.then(cachedResponse => {
+if (cachedResponse) {
+return cachedResponse;
+}
 
-        return fetch(request).then(response => {
-          if (!response || response.status !== 200) {
-            return response;
-          }
+```
+    return fetch(request).then(response => {
+      if (!response || response.status !== 200) {
+        return response;
+      }
 
-          const responseClone = response.clone();
+      const responseClone = response.clone();
 
-          caches.open(CACHE_NAME).then(cache => {
-            cache.put(request, responseClone);
-          });
-
-          return response;
+      caches.open(CACHE_NAME)
+        .then(cache => {
+          cache.put(request, responseClone);
         });
-      })
-  );
+
+      return response;
+    });
+  })
+```
+
+);
 });
